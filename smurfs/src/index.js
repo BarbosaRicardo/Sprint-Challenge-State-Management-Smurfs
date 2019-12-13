21f869c3f5040325162322119e8e5157e447e860
Ricardo <ricardo@Ricardos-Mac-mini.local>
2019-12-13 08:51:04 -0800

    reducer staged plus imported to src/index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+//import createStore and applyMiddleware
+import { createStore, applyMiddleware } from 'redux';
+import { Povider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { smurfReducer } from './reducers/smurfReducer.js'
+
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+    smurfReducer,
+    applyMiddleware(thunk)
+);
+
+
+//wrap App with Provider 
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById("root"));
---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,14 @@
+const initialState ={
+
+}
+
+export function smurfReducer(state = initialState, action) {
+    switch(action.type) {
+        case 'GET_SMURFS':
+            return {
+                smurfs: action.payload
+            }
+
+            default: return state;
+    }
+}
