04d6d26197d69a5ba23ccbac74277afdb8eb712d
Ricardo <ricardo@Ricardos-Mac-mini.local>
2019-12-13 10:02:52 -0800

    got as far as I know

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import './App.css'
 import { getSmurfs } from '../actions/getSmurfs.js/index.js'
 
+
 function App(props) {
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
@@ -43,7 +44,8 @@ function App(props) {
   return (
     <div className ="App">
       <h1>Smurfs! 2.0 W/Redux</h1>
-
+      
+      
       <form onSubmit={handleSubmit}>
         <input placeholder ='Name' name='Name' value={name} onChange={handleNameChange} />
         <input placeholder = 'Age' age='Age' value={age} onChange={handleAgeChange} />
@@ -71,7 +73,8 @@ function mapStateToProps(state) {
 }
 
 const mapDispatchToProps = {
-  getSmurfs
+  getSmurfs,
+  postSmurf
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
