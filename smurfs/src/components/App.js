46fd93e4ec1f8e00fa50d1f9a336dd04b6d328d9
Ricardo Barbosa <ric3639@gmail.com>
2020-05-04 17:37:57 -0700

    mvp with context api

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,82 +1,32 @@
-import React, { useState, useEffect } from "react";
-import { connect } from 'react-redux'
-import './App.css'
-import { getSmurfs } from '../actions/getSmurfs.js'
-import { postSmurf } from '../actions/postSmurf.js'
+import React, { Component, useState, useEffect } from "react";
+import "./App.css";
+import { SmurfContext } from "../contexts/SmurfContext";
+import SmurfList from "./SmurfList";
+import axios from "axios";
+import Form from "./Form";
 
-
-function App(props) {
-  const [name, setName] = useState('');
-  const [age, setAge] = useState('');
-  const [height, setHeight] = useState('');
+function App() {
+  const [smurfs, setSmurfs] = useState();
 
   useEffect(() => {
-    props.getSmurfs();
-  }, [])
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    props.postSmurf({
-      name: name,
-      age: age,
-      height: height,
-      id: Date.now()
-    })
-    setName('')
-    setAge('')
-    setHeight('')
-  }; //end of handleSubmit 
-
-  const handleNameChange = event => {
-    event.preventDefault();
-    setName(event.target.value)
-  }; //end of handleNameChange
-
-  const handleAgeChange = event => {
-    event.preventDefault();
-    setAge(event.target.value)
-  }; //end of handleAgeChange
-
-  const handleHeightChange = event => {
-    event.preventDefault();
-    setHeight(event.target.value)
-  }; //end of handleHeightChange
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => {
+        // console.log(res.data);
+        setSmurfs(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
 
   return (
-    <div className ="App">
-      <h1>Smurfs! 2.0 W/Redux</h1>
-      
-      
-      <form onSubmit={handleSubmit}>
-        <input placeholder ='Name' name='Name' value={name} onChange={handleNameChange} />
-        <input placeholder = 'Age' age='Age' value={age} onChange={handleAgeChange} />
-        <input placeholder ='Height' height='Height' value={height} onChange={handleHeightChange} />
-        <button onClick={handleSubmit}>Submit</button>
-      </form>
-
-      <div className='smurf-cards'>
-        {props.smurfs.map((smurf) => {
-          return <div className='smurf-card' key={smurf.id}>
-            <h2>{smurf.name}</h2>
-            <h3>Age: {smurf.age}</h3>
-            <h3>Height: {smurf.height}</h3>
-      </div>
-        })}
-     </div>
+    <div className="App">
+      <SmurfContext.Provider value={{ smurfs }}>
+        <h1>SMURFS! 2.0 W/ Redux</h1>
+        <Form />
+        <SmurfList />
+      </SmurfContext.Provider>
     </div>
-  ); //end of return statement 
-}; //end of App
-
-
-function mapStateToProps(state) {
-  return {
-    smurfs: state.smurfs
-  }
+  );
 }
 
-const mapDispatchToProps = {
-  getSmurfs,
-  postSmurf
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,23 +1,13 @@
-import React from "react";
+
+import React from 'react';
 import ReactDOM from "react-dom";
-
-//import createStore and applyMiddleware
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { smurfReducer } from './reducers/smurfReducer.js'
-
 import "./index.css";
 import App from "./components/App";
+import { BrowserRouter as Router } from "react-router-dom";
+// import { createContext } from "react";
 
-const store = createStore(
-    smurfReducer,
-    applyMiddleware(thunk)
-);
-
-
-//wrap App with Provider 
 ReactDOM.render(
-<Provider store={store}>
-<App />
-</Provider>, document.getElementById("root"));
+    <Router>
+        <App />
+    </Router>, document.getElementById('root')
+); 
